19feb04c66f33edd651b3ce5a16119638cb898f9
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2020-01-20 14:10:37 -0600

    Update deleteComputerID.sh

--- API/deleteComputerID.sh
+++ API/deleteComputerID.sh
@@ -14,6 +14,34 @@
 # Delete computer by ID
 #
 ####################################################################################################
+# Courtesy of github dot com slash zdorow
+echo "Testing connection to Jamf Pro..."
+echo "This test assumes the user has admin credentials"
+test=$(curl --fail -ksu "$username":"$password" "$server"/JSSResource/users -X GET)
+status=$?
+if [ $status -eq 6 ]; then
+	echo ""
+	echo "The Jamf Pro URL is incorrect. Please edit the URL and try again."
+	echo "If the error persists please check permissions and internet connection"
+	echo ""
+	exit 99
+elif [ $status -eq 22 ]; then
+	echo ""
+	echo "Username and/or password is incorrect. Please edit and try again."
+	echo "If the error persists please check permissions and internet connection"
+	echo ""
+	exit 99
+elif [ $status -eq 0 ]; then
+    echo ""
+    echo "Connection test successful! "
+    echo ""
+else
+    echo ""
+    echo "Something went really wrong,"
+    echo "Lets try this again."
+    exit 99
+fi
+####################################################################################################
 #
 # DEFINE VARIABLES & READ IN PARAMETERS
 #
