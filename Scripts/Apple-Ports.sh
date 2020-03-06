c5d73e34f4884a9f441437d30a8116e0d5aafb2b
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2020-03-06 14:53:28 -0600

    Add files via upload

--- Scripts/Apple-Ports.sh
+++ Scripts/Apple-Ports.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 output="/tmp/ports.txt"
-touch "$output"
+if [[ ! -f $output ]]; then
+   echo "File does not exist, creating now!"
+   touch $output
+else
+   echo "File Exits, removing now!"
+   rm $output
+fi
 echo "--Various Ports--" >> "$output"
 nc -vz gateway.push.apple.com 2195 >> "$output" 2>&1
 nc -vz gateway.sandbox.push.apple.com 2195 >> "$output" 2>&1
@@ -21,6 +27,11 @@ nc -vz mesu.apple.com 80 >> "$output" 2>&1
 nc -vz swscan.apple.com 80 >> "$output" 2>&1
 nc -vz xp.apple.com 80 >> "$output" 2>&1
 nc -vz configuration.apple.com 80 >> "$output" 2>&1
+nc -vz captive.apple.com 80 >> "$output" 2>&1
+nc -vz static.ips.apple.com 80 >> "$output" 2>&1
+nc -vz crl3.digicert.com 80 >> "$output" 2>&1
+nc -vz crl4.digicert.com 80 >> "$output" 2>&1
+nc -vz ocsp.digicert.com 80 >> "$output" 2>&1
 echo "--Port 443 Below --" >> "$output" 2>&1
 nc -vz hrweb.cdn-apple.com 443 >> "$output" 2>&1
 nc -vz itunes.apple.com 443 >> "$output" 2>&1
@@ -30,10 +41,24 @@ nc -vz appldnld.apple.com 443 >> "$output" 2>&1
 nc -vz gg.apple.com 443 >> "$output" 2>&1
 nc -vz gs.apple.com 443 >> "$output" 2>&1
 nc -vz itunes.apple.com 443 >> "$output" 2>&1 2>&1
-nc -vz skl.apple.com 443 >> "$output" 2>&1 2>&1
+nc -vz skl.apple.com 443 >> "$output" 2>&1
 nc -vz mesu.apple.com 443 >> "$output" 2>&1
 nc -vz swscan.apple.com 443 >> "$output" 2>&1
 nc -vz xp.apple.com 443 >> "$output" 2>&1
 nc -vz configuration.apple.com 443 >> "$output" 2>&1
-
- echo "Done"
+nc -vz captive.apple.com 443 >> "$output" 2>&1
+nc -vz humb.apple.com 443 >> "$output" 2>&1
+nc -vz static.ips.apple.com 443 >> "$output" 2>&1
+nc -vz tbsc.apple.com 443 >> "$output" 2>&1
+nc -vz gdmf.apple.com 443 >> "$output" 2>&1
+nc -vz deviceenrollment.apple.com 443 >> "$output" 2>&1
+nc -vz deviceservices-external.apple.com 443 >> "$output" 2>&1
+nc -vz identity.apple.com 443 >> "$output" 2>&1
+nc -vz iprofiles.apple.com 443 >> "$output" 2>&1
+nc -vz mdmenrollment.apple.com 443 >> "$output" 2>&1
+nc -vz vpp.itunes.apple.com 443 >> "$output" 2>&1
+nc -vz ppq.apple.com 443 >> "$output" 2>&1
+nc -vz crl3.digicert.com 443 >> "$output" 2>&1
+nc -vz crl4.digicert.com 443 >> "$output" 2>&1
+nc -vz ocsp.digicert.com 443 >> "$output" 2>&1
+echo "Done"
