601e56983a448118a8908ade34e41cc76521bdf5
yoopersteeze <ryan.peterson1@icloud.com>
2020-03-13 12:00:27 -0500

    EA for Determining if the Cisco WebEx Chrome Extension is installed. chrome://extensions/?id=jlhmfgmfgeifomenelglieieghnjghma is installed.

---
+++ EAs/ciscoWebExChromeExtensionInstalled.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Detects if CiscoWebEx Chrome Extension is installed
+# chrome://extensions/?id=jlhmfgmfgeifomenelglieieghnjghma
+
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+ext="/Users/$loggedInUser/Library/Application Support/Google/Chrome/Default/Extensions/jlhmfgmfgeifomenelglieieghnjghma/"
+if [[ ! -d "$ext" ]]; then
+  echo "<result>Not Installed</result>"
+else
+  echo "<result>Installed</result>"
+fi
