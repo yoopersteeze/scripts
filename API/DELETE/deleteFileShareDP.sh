d63b15c70980f9b5f579e9c4d49286a9e305e847
yoopersteeze <ryan.peterson1@icloud.com>
2020-04-01 09:33:17 -0500

    ...

---
+++ API/DELETE/deleteFileShareDP.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+####################################################################################################
+#
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT
+#
+####################################################################################################
+#
+# DESCRIPTION
+# Delete file share in Jamf Pro
+#
+####################################################################################################
+#
+# DEFINE VARIABLES & READ IN PARAMETERS
+#
+####################################################################################################
+
+read -p "Jamf Pro URL: " server
+read -p "Jamf Pro Username: " username
+read -s -p "Jamf Pro Password: " password
+echo ""
+read -p "File Share ID " id
+####################################################################################################
+#
+# MAIN PROCESS
+#
+####################################################################################################
+curl -ksu "$username":"$password" -H "content-type: text/xml" "$server"/JSSResource/distributionpoints/id/$id -X DELETE
