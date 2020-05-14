1c8dd5c137968f75533f942d18de12a77e0c19ba
yoopersteeze <ryan.peterson1@icloud.com>
2020-05-14 15:00:59 -0500

    init commit

---
+++ API/PUT/updateAC.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+####################################################################################################
+#
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT
+#
+####################################################################################################
+#
+# DESCRIPTION
+# Update AC in Jamf Pro
+#
+####################################################################################################
+#
+# DEFINE VARIABLES & READ IN PARAMETERS
+#
+####################################################################################################
+
+read -p "Jamf Pro URL: " server
+read -p "Jamf Pro Username: " username
+read -s -p "Jamf Pro Password: " password
+echo ""
+read -p "Activation Code: " code
+
+####################################################################################################
+#
+# SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
+#
+####################################################################################################
+#
+# MAIN PROCESS
+#
+####################################################################################################
+curl -ksu "$username":"$password" -H "content-type: text/xml" "$server"/JSSResource/activationcode -X PUT -d '<activation_code><code>$code</code></activation_code>'
