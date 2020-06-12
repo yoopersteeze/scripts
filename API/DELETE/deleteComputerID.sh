c7447349f3dcd7da356e23553c2ef8ed2fdfe11f
yoopersteeze <ryan.peterson1@icloud.com>
2020-06-12 12:14:18 -0500

    removed auth check

--- API/DELETE/deleteComputerID.sh
+++ API/DELETE/deleteComputerID.sh
@@ -26,38 +26,10 @@ read -s -p "Jamf Pro Password: " password
 echo ""
 read -p "ComputerID: " cid
 ####################################################################################################
-# Courtesy of github dot com slash zdorow
-echo "Testing connection to Jamf Pro..."
-echo "This test assumes the user has admin credentials"
-test=$(curl --fail -ksu "$username":"$password" "$server"/JSSResource/users -X GET)
-status=$?
-if [ $status -eq 6 ]; then
-	echo ""
-	echo "The Jamf Pro URL is incorrect. Please edit the URL and try again."
-	echo "If the error persists please check permissions and internet connection"
-	echo ""
-	exit 99
-elif [ $status -eq 22 ]; then
-	echo ""
-	echo "Username and/or password is incorrect. Please edit and try again."
-	echo "If the error persists please check permissions and internet connection"
-	echo ""
-	exit 99
-elif [ $status -eq 0 ]; then
-    echo ""
-    echo "Connection test successful! "
-    echo ""
-else
-    echo ""
-    echo "Something went really wrong,"
-    echo "Lets try this again."
-    exit 99
-fi
 ####################################################################################################
 #
 # SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
 #
 ####################################################################################################
-
 
 curl -ksu "$username":"$password" -H "content-type: text/xml" "$server"/JSSResource/computers/id/$cid -X DELETE
