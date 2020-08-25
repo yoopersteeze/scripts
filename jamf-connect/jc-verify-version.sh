671ea3fdc70d84e7db4fb7230fe4775fabf23521
yoopersteeze <ryan.peterson1@icloud.com>
2020-08-25 17:23:23 -0500

    init commit

---
+++ jamf-connect/jc-verify-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+jcvv=$(defaults read /Applications/Jamf\ Connect\ Verify.app/Contents/Info.plist CFBundleShortVersionString)
+file="/Applications/Jamf\ Connect\ Verify.app/Contents/Info.plist"
+if [[ ! -e $file ]]; then
+  echo "<result>Not Installed</result>"
+else
+echo "<result>$jcvv</result>"
+fi
