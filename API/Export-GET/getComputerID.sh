140e07fb1682a7deade26027d47a68334b0a8f0d
yoopersteeze <ryan.peterson1@icloud.com>
2020-08-26 14:26:01 -0500

    init commit

---
+++ API/Export-GET/getComputerID.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+####################################################################################################
+# 
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT                                                                                                                         
+# 
+####################################################################################################
+#
+# DESCRIPTION
+# curl a single computer
+#	
+####################################################################################################
+#
+# DEFINE VARIABLES & READ IN PARAMETERS
+#
+####################################################################################################
+
+read -p "Jamf Pro URL: " server
+read -p "Jamf Pro Username: " username
+read -s -p "Jamf Pro Password: " password
+echo ""
+read -p "Computer ID: " cid
+####################################################################################################
+#
+# SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
+#
+####################################################################################################
+
+curl -ksu "$username":"$password" -H "accept: text/xml" "$server"/JSSResource/computers/id/$cid | xmllint --format -
