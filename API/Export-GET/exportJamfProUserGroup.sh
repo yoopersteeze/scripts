97e1c6161210695a2726f9dbbcdab5fcdfa7b705
yoopersteeze <ryan.peterson1@icloud.com>
2020-11-05 10:24:05 -0600

    init commit

--- API/Export-GET/exportJamfProUserGroup.sh
+++ API/Export-GET/exportJamfProUserGroup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+
 ####################################################################################################
-# 
+#
 # THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
 # AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
 #
 # BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
 # IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
-# MAINTAIN THIS SCRIPT                                                                                                                         
-# 
+# MAINTAIN THIS SCRIPT
+#
 ####################################################################################################
 #
 # DESCRIPTION
-# curl a single computer
-#	
+# This script will export a jamf pro user group  as xml to the Desktop called "Jamf-Pro-User-Group-$ID.xml"
+#
 ####################################################################################################
 #
 # DEFINE VARIABLES & READ IN PARAMETERS
@@ -23,11 +24,13 @@ read -p "Jamf Pro URL: " server
 read -p "Jamf Pro Username: " username
 read -s -p "Jamf Pro Password: " password
 echo ""
-read -p "Computer ID: " cid
+read -p "Jamf Pro User Group ID: " id
 ####################################################################################################
 #
-# SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
+# MAIN PROCESS
 #
 ####################################################################################################
-
-curl -ksu "$username":"$password" -H "accept: text/xml" "$server"/JSSResource/computers/id/$cid | xmllint --format - #> ~/Downloads/file.txt
+loggedInUser=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+echo "Exporting Jamf Pro User ID: $id to Desktop"
+curl -ksu $username:$password -H "accept: text/xml" $server/JSSResource/accounts/groupid/$id | xmllint -format - > /Users/"$loggedInUser"/Desktop/Jamf-Pro-User-Group-$id.xml
+echo "Done!"
