109f51c2f3324bc5d5c4279ce46ce7dca029f45c
yoopersteeze <ryan.peterson1@icloud.com>
2020-12-29 13:12:56 -0600

    removed auth check

--- SetComputerName/setComputerName-AssetTag.sh
+++ SetComputerName/setComputerName-AssetTag.sh
@@ -36,32 +36,6 @@ if [ $(echo "${server: -1}") == "/" ]; then
 	jssURL=$(echo $server | sed 's/.$//')
 fi
 ####################################################################################################
-# Courtesy of github dot com slash zdorow
-echo "Testing connection to Jamf Pro..."
-test=$(curl --fail -ksu "$username":"$password" "$server"/JSSResource/users -X GET)
-status=$?
-if [ $status -eq 6 ]; then
-	echo ""
-	echo "The Jamf Pro URL is incorrect. Please edit the URL and try again."
-	echo "If the error persists please check permissions and internet connection"
-	echo ""
-	exit 99
-elif [ $status -eq 22 ]; then
-	echo ""
-	echo "Username and/or password is incorrect. Please edit and try again."
-	echo "If the error persists please check permissions and internet connection"
-	echo ""
-	exit 99
-elif [ $status -eq 0 ]; then
-    echo ""
-    echo "Connection test successful! "
-    echo ""
-else
-    echo ""
-    echo "Something went really wrong,"
-    echo "Lets try this again."
-    exit 99
-fi
 # Get the Serial Number
 serialNumber=$(ioreg -l | grep IOPlatformSerialNumber | awk '{print $4}' | tr -d \")
 echo "Serial Number is: $serialNumber"
