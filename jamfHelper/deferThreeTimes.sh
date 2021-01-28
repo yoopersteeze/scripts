6eb543db80c7a44724ab8a27b783210e5bf8e6bb
yoopersteeze <ryan.peterson1@loud.com>
2021-01-28 10:04:48 -0600

    updated EULA

--- jamfHelper/deferThreeTimes.sh
+++ jamfHelper/deferThreeTimes.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# Credits to github dot com slash zacharyfisher 
+####################################################################################################
+#
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT
+#
+####################################################################################################
+# Credits to github dot com slash zacharyfisher
 #####################################
 # Set Variables
 #####################################
