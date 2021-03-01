6da759e9e207e429e7cbaa002ef71306b0866285
yoopersteeze <ryan.peterson1@icloud.com>
2021-03-01 12:38:55 -0600

    init commit

---
+++ API/Export-GET/isComputerManaged-CSV.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+####################################################################################################
+# 
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT                                                                                                                         
+# 
+####################################################################################################
+#
+# DESCRIPTION
+# Read in CSV of computers and echo if managed
+#	
+####################################################################################################
+#
+# DEFINE VARIABLES & READ IN PARAMETERS
+#
+####################################################################################################
+
+read -p "Jamf Pro URL: " server
+read -p "Jamf Pro Username: " username
+read -s -p "Jamf Pro Password: " password
+echo ""
+read -p "Please drag and drop the csv into this window and hit enter: " computerList
+
+####################################################################################################
+
+####################################################################################################
+#
+# SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
+#
+####################################################################################################
+
+# Courtesy of github dot com slash iMatthewCM
+#Read CSV into array
+IFS=$'\n' read -d '' -r -a computerIDs < $computerList
+
+length=${#computerIDs[@]}
+
+#Do all the things
+for ((i=0; i<$length;i++));
+	
+do
+	id=$(echo ${computerIDs[i]} | sed 's/,//g' | sed 's/ //g'| tr -d '\r\n')
+	get=$(curl -ksu "$username":"$password" -H "content-type: text/xml" "$server"/JSSResource/computers/id/"$id" | xmllint --xpath '/computer/general/remote_management/managed/text()' - )
+	echo "ID $id is Managed: $get"
+done
