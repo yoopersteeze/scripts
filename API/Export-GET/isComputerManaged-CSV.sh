91d7ea7d2118ff02d14f8880fe63fa7aed36d6fd
yoopersteeze <ryan.peterson@icloud.com>
2021-03-05 09:05:48 -0600

    added logic for MDM Capable

--- API/Export-GET/isComputerManaged-CSV.sh
+++ API/Export-GET/isComputerManaged-CSV.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ####################################################################################################
-# 
+#
 # THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
 # AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
 #
 # BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
 # IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
-# MAINTAIN THIS SCRIPT                                                                                                                         
-# 
+# MAINTAIN THIS SCRIPT
+#
 ####################################################################################################
 #
 # DESCRIPTION
 # Read in CSV of computers and echo if managed
-#	
+#
 ####################################################################################################
 #
 # DEFINE VARIABLES & READ IN PARAMETERS
@@ -41,9 +41,12 @@ length=${#computerIDs[@]}
 
 #Do all the things
 for ((i=0; i<$length;i++));
-	
+
 do
 	id=$(echo ${computerIDs[i]} | sed 's/,//g' | sed 's/ //g'| tr -d '\r\n')
-	get=$(curl -ksu "$username":"$password" -H "content-type: text/xml" "$server"/JSSResource/computers/id/"$id" | xmllint --xpath '/computer/general/remote_management/managed/text()' - )
-	echo "ID $id is Managed: $get"
+	managed=$(curl -ksu "$username":"$password" -H "content-type: text/xml" "$server"/JSSResource/computers/id/"$id" | xmllint --xpath '/computer/general/remote_management/managed/text()' - )
+	mdm=$(curl -ksu "$username":"$password" -H "content-type: text/xml" "$server"/JSSResource/computers/id/"$id" | xmllint --xpath '/computer/general/mdm_capable/text()' - )
+	echo "ID $id is:
+	Managed: $managed
+	MDM Capable: $mdm"
 done
