045afadca19f33b0f09ee0f72e32de624c84bc48
yoopersteeze <ryan.peterson1@icloud.com>
2021-05-26 17:25:51 -0500

    init commit

---
+++ Scripts/resetDiskPermissions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# get logged in user
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+
+# bail out if user is null, root or _mbsetupser
+if [[ -z $loggedInUser || "$loggedInUser" == "root" || "$loggedInUser" == "_mbsetupser" ]]; then
+    echo "No real user logged in"
+    echo "Logged in user is: $loggedInUser"
+    exit 1
+else
+  # run the command we need
+/usr/sbin/diskutil resetUserPermissions / $(id -u $loggedInUser)
+fi
